337e97c5905ff67c439d63415270b9fd5b651f5c
platonovAlexey <kalyawe4ka@gmail.com>
2019-01-04 01:05:15 +0300

    add file

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-footer :fixed="true" app>
+            <span>Learning German &copy; 2019</span>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+      <v-navigation-drawer absolute temporary v-model="drawer" class="hidden-md-and-up">
+        <v-list>
+          <v-list-tile v-for="(item, i) in menuItems" :key="`navdrawer${i}`">
+            <v-list-tile-action>
+              <v-icon left v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+                <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app dark class="primary">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          <v-toolbar-title v-text="'Learning German'"></v-toolbar-title>
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat v-for="(item, i) in menuItems" 
+                :key="`menuItems${i}`" 
+                :to="item.route">
+          <v-icon left v-html="item.icon"></v-icon>
+          {{item.title}}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        data () {
+          return {
+            drawer: false
+          }
+        },
+        computed: {
+          menuItems () {
+            return [
+              {
+                icon: 'visibility',
+                title: 'Читать',
+                route: '/books'
+              },
+              {
+                icon: 'extension',
+                title: 'Учить слова',
+                route: '/words'
+              },
+              {
+                icon: 'account_circle',
+                title: 'Мой кабинет',
+                route: '/profile'
+              },
+              {
+                icon: 'exit_to_app',
+                title: 'Выйти',
+                route: '/logout'
+              },
+              {
+                icon: 'input',
+                title: 'Войти',
+                route: '/signin'
+              },
+              {
+                icon: 'lock_open',
+                title: 'Зарегистрироваться',
+                route: '/signup'
+              }
+            ]
+          }
+        }
+    }
+</script>
