e4f46928df9f664ae9f0f920214f08738fa04929
platonovAlexey <kalyawe4ka@gmail.com>
2019-01-04 01:00:41 +0300

    first commit, created structure

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Books from '@/components/Books.vue';
+import Words from '@/components/Words.vue';
+import Profile from '@/components/Profile.vue';
+import Signin from '@/components/Signin.vue';
+import Signup from '@/components/Signup.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -12,12 +18,29 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/books',
+      name: 'books',
+      component: Books,
+    },
+    {
+      path: '/words',
+      name: 'words',
+      component: Words,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: Signin,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
     },
   ],
 });
